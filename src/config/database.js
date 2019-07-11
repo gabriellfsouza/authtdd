e11d7d42fb20f10a706dac4cf8299f0db411c15e
Gabriel de Souza <gabriellfsouza@gmail.com>
2019-07-11 01:14:38 -0300

    Revert "chore: inclusão de teste para validar login com credenciais válidas"
    This reverts commit 7320b9918ab04dea63ca5924aabe2aa06ff7ac2e.

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,9 @@
-require("dotenv").config({
-  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
-});
-
 module.exports = {
-  dialect: process.env.DB_DIALECT || "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  storage: "./__tests__/database.sqlite",
+  dialect: "postgres",
+  host: "127.0.0.1",
+  username: "docker",
+  password: "docker",
+  database: "nodeauth",
   operatorsAliases: false,
   loging: false,
   define: {
